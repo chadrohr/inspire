fd112d36b71c031dc002b245bfce7819c633aae4
Chad Rohr <chadrohr@yahoo.com>
2016-10-14 10:01:50 -0600

    Start

--- app/components/quote/quote-controller.js
+++ app/components/quote/quote-controller.js
@@ -1,5 +1,16 @@
 (function(){
-	
 	//Get your QuoteService
+	function QuoteController (){
+		var quoteController = new QuoteController
+
+		quoteController.quoteOfTheDay()
+			.then(function (data){
+				var template = $('#quoteTemplate').html();
+
+				template = template.replace('{{ ??? }}', data.???)
+				template = template.replace("{{ ??? }}"), data.???)
+			})
+	}
+	QuoteController()
 	
 }())
--- app/components/quote/quote-service.js
+++ app/components/quote/quote-service.js
@@ -10,4 +10,5 @@ function QuoteService(){
 			//Now What?
 		})
 	}
+	
 }
