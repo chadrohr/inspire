f20e570f1b96f5f099bf8da5f81515617c80c732
Chad Rohr <chadrohr@yahoo.com>
2016-10-16 12:54:57 -0600

    sunday sync

--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -1,17 +1,17 @@
-(function (){
-	
+(function () {
+
 	var wc = this;
 	var weatherService = new WeatherService();
-	
-	weatherService.getWeather(function(weather){
+
+	weatherService.getWeather(function (weather) {
 		//console.log(weather);
-	   	weather = JSON.parse(weather)
+		weather = JSON.parse(weather)
 		//debugger//What can you do with this weather object?
 		var place = weather.name
 		var temp = parseInt(weather.main.temp)
 		var condition = weather.weather[0].description
 		var celTemp = temp - 273
-		var ferTemp = parseInt(celTemp * 1.8 + 32) 
+		var ferTemp = parseInt(celTemp * 1.8 + 32)
 		//console.log(place, temp, celTemp, ferTemp, condition)
 		weatherElem = $('#weather');
 		var template = `<h1>${place}</h1>
@@ -19,17 +19,17 @@
 						<h3>${celTemp}&degC;</h3>
 						<h5>${condition}</h5>
 						`
-						weatherElem.append(template)
-})
-	
-		
-	
-	
-	
-}())
+		weatherElem.append(template)
+	})
+
+
+
+
+
+} ())
 
 // <div class="card text-center">
 //             <p>${wc.weather.name}</p>
 //             <p>${wc.weather.main.temp}</p>
-            
+
 //         </div>
