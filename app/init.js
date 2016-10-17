e05fa3196df25dfa74adb77ed4b3c9e6f07cc507
Chad Rohr <chadrohr@yahoo.com>
2016-10-17 10:41:23 -0600

    changed clock

--- app/init.js
+++ app/init.js
@@ -1,4 +1,4 @@
-<script>
+
 function startTime() {
     var today = new Date();
     var h = today.getHours();
@@ -14,5 +14,5 @@ function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
-</script>
-<body onload="startTime()">
+
+startTime()
